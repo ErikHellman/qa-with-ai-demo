b5cc3b8fce95c8650e582dd2315a1c85ef0369e0
Erik Hellman <erik.hellman@husqvarnagroup.com>
2025-05-16 18:02:15 +0200

    Change function name

--- src/fn.js
+++ src/fn.js
@@ -1,9 +1,9 @@
-function fn(input) {
-  if (!Array.isArray(input) || input.length < 3) {
+function shoelaceFormula(points) {
+  if (!Array.isArray(points) || points.length < 3) {
     throw new Error('Bad input');
   }
 
-  for (const b of input) {
+  for (const b of points) {
     if (!Array.isArray(b) || b.length !== 2 || 
         typeof b[0] !== 'number' || typeof b[1] !== 'number') {
       throw new Error('Bad format');
@@ -11,15 +11,15 @@ function fn(input) {
   }
 
   let m = 0;
-  const n = input.length;
+  const n = points.length;
 
   for (let i = 0; i < n; i++) {
     const j = (i + 1) % n;
-    m += input[i][0] * input[j][1];
-    m -= input[j][0] * input[i][1];
+    m += points[i][0] * points[j][1];
+    m -= points[j][0] * points[i][1];
   }
 
   return Math.abs(m) / 2;
 }
 
-module.exports = { fn: fn };
+module.exports = { fn: shoelaceFormula };
